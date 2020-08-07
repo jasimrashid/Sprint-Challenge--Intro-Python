18181c111614e52ea78da91147886f761d7e2df4
Jasim Rashid <jasim.rashid@gmail.com>
2020-08-07 21:58:09 +0530

    initial commit, WIP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,61 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # This is the base class
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+# class Vehicle:
+#     # This is the base class
+#     def __init__(self, name):
+#         self.name = name
+
+# class GroundVehicle(Vehicle):
+#     def __init__(self,name):
+#         super().__init__(name)
+
+# class Car(GroundVehicle):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+# class Motorcycle(GroundVehicle):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+# class FlightVehicle(Vehicle):
+#     def __init__(self,name):
+#         super().__init__(name)
+
+# class Airplane(FlightVehicle):
+#     def __init__(self,name):
+#         super().__init__(name)
+
+# class Starship(FlightVehicle):
+#     def __init__(self,name):
+#         super().__init__(name)
