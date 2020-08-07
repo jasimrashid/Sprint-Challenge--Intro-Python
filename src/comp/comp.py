0707cbb1de831393f0c57c7944ebc85b3b14e9d9
Jasim Rashid <jasim.rashid@gmail.com>
2020-08-07 22:07:16 +0530

    MVP!

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -67,7 +67,7 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
-f = [str("('"+i.name+"', "+str(i.age)+")") for i in humans if i.age>=27 and i.age<=32]
+f = [(i.name, int(i.age)) for i in humans if i.age>=27 and i.age<=32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -75,7 +75,7 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names uppercase:")
 g = []
-g = [str("("+i.name.upper()+", "+str(i.age+5)+")") for i in humans if i.age>=27 and i.age<=32]
+g = [Human(i.name.upper(), int(i.age)+5) for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
